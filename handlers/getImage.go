683a5d3079dfe23981567705d41bec4b61cad078
Vathsan <srivaths311@gmail.com>
2019-08-12 11:10:52 +0530

    Boilerplate v2

--- handlers/getImage.go
+++ handlers/getImage.go
@@ -3,9 +3,12 @@ package handlers
 import (
     "net/http"
     "fmt"
+    "github.com/gorilla/mux"
     //"encoding/json"
 )
 
 func GetImage(w http.ResponseWriter, r *http.Request)  {
     fmt.Println("GET GetImage")
+    params := mux.Vars(r)
+    fmt.Println(params["id"])
 }
--- handlers/getMember.go
+++ handlers/getMember.go
@@ -3,10 +3,12 @@ package handlers
 import (
     "net/http"
     "fmt"
+    "github.com/gorilla/mux"
     //"encoding/json"
 )
 
 func GetMember(w http.ResponseWriter, r *http.Request)  {
     fmt.Println("GET GetMember")
-    
+    params := mux.Vars(r)
+    fmt.Println(params["id"])
 }
